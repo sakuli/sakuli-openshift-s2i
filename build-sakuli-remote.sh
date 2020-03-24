9475e53fa231b60f8368f6c928e4e3bd0e24fe25
Van Cliff Le <cliff.le.van@consol.de>
2020-03-24 14:05:13 +0100

    Release 2.3.0-1

--- build-sakuli-remote.sh
+++ build-sakuli-remote.sh
@@ -4,4 +4,4 @@ make build test prepare-release \
     IMAGE_NAME=sakuli-s2i-remote-connection \
     BASE_IMAGE=sakuli-remote-connection \
     BASE_IMAGE_VERSION=2.3.0 \
-    TAG_VERSION=2.3.0
+    TAG_VERSION=2.3.0-1
