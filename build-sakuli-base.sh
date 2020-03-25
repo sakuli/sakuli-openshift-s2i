37a6b7c67151a4972bdb8ddf9014a863fb0bdfa2
Van Cliff Le <cliff.le.van@consol.de>
2020-03-25 16:04:41 +0100

    Release 2.3.0-2

--- build-sakuli-base.sh
+++ build-sakuli-base.sh
@@ -4,4 +4,4 @@ make build test prepare-release \
     IMAGE_NAME=sakuli-s2i \
     BASE_IMAGE=sakuli \
     BASE_IMAGE_VERSION=2.3.0 \
-    TAG_VERSION=2.3.0-1
+    TAG_VERSION=2.3.0-2
--- release.sh
+++ release.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-export VERSION="2.3.0-1"
+export VERSION="2.3.0-2"
 
 make release IMAGE_NAME=sakuli-s2i TAG_VERSION=${VERSION}
 make release IMAGE_NAME=sakuli-s2i-remote-connection TAG_VERSION=${VERSION}
