82bc7d7238e19e53bf61b9f6e3bb5bf4ef0387c8
Simon Hofmann <simon.hofmann@consol.de>
2020-09-01 14:52:00 +0200

    Release 2.4.0

--- build-sakuli-base.sh
+++ build-sakuli-base.sh
@@ -3,5 +3,5 @@
 make build test prepare-release \
     IMAGE_NAME=sakuli-s2i \
     BASE_IMAGE=sakuli \
-    BASE_IMAGE_VERSION=2.3.0 \
-    TAG_VERSION=2.3.0-2
+    BASE_IMAGE_VERSION=2.4.0 \
+    TAG_VERSION=2.4.0
--- build-sakuli-remote.sh
+++ build-sakuli-remote.sh
@@ -3,5 +3,5 @@
 make build test prepare-release \
     IMAGE_NAME=sakuli-s2i-remote-connection \
     BASE_IMAGE=sakuli-remote-connection \
-    BASE_IMAGE_VERSION=2.3.0 \
-    TAG_VERSION=2.3.0-2
+    BASE_IMAGE_VERSION=2.4.0 \
+    TAG_VERSION=2.4.0
